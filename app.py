0d26bb845bc35b51f8caea8b7cde13a674a63c9d
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-18 19:27:55 +0530

    setup --- db initialisation

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 from Config import Config
+from database.db import init_db
 
 api = FastAPI(debug=Config.debug)
 
+@api.on_event("startup")
+async def start_db():
+    await init_db()
+
+@api.get("/")
+async def get_docs():
+    return RedirectResponse("/docs")
+
 if __name__ == "__main__":
-    uvicorn.run("app:api",reload=Config.debug)
+    uvicorn.run("app:api", reload=Config.debug)
--- database/db.py
+++ database/db.py
@@ -1,5 +1,9 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient 
 from Config import Config
+from beanie import init_beanie
+from .models import models
 
-client = AsyncIOMotorClient(Config.db_uri)
-print(client)
+
+async def init_db():
+    client = AsyncIOMotorClient(Config.db_uri)
+    await init_beanie(database=client.devsoc,document_models=models)
