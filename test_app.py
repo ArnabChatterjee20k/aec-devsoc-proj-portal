406e500215bfdeea98bd1642d9fd18bc8fe6c4db
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-18 22:07:54 +0530

    test -- added tests for auth helpers

--- test_app.py
+++ test_app.py
@@ -1,9 +1,32 @@
 from fastapi.testclient import TestClient
 from fastapi import status
 from app import api
+from utils.auth import create_access_token , decode_access_token , get_password_hash , verify_password
 
 client = TestClient(app=api)
 
 def test_docs_redirection():
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
+
+def test_create_access_token():
+    subject = "user123"
+    token = create_access_token(subject)
+    assert isinstance(token, str)
+
+def test_decode_access_token():
+    token = create_access_token("test_")
+    payload = decode_access_token(token)
+    assert isinstance(payload, dict)
+    assert "sub" in payload
+
+def test_verify_password():
+    plain_password = "password123"
+    hashed_password = get_password_hash(plain_password)
+    result = verify_password(plain_password, hashed_password)
+    assert result is True
+
+def test_get_password_hash():
+    plain_password = "password123"
+    hashed_password = get_password_hash(plain_password)
+    assert isinstance(hashed_password, str)
---
+++ utils/auth.py
@@ -0,0 +1,52 @@
+from fastapi import HTTPException
+from datetime import datetime, timedelta
+from typing import Any, Union
+
+import jwt
+from passlib.context import CryptContext
+
+from Config import Config
+from constants.status import FAIL
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: Union[str, Any]) -> str:
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=Config.access_token_expire_days),
+        'iat': datetime.utcnow(),
+        'sub': subject
+    }
+    return jwt.encode(
+        payload,
+        Config.secret,
+        algorithm=ALGORITHM
+    )
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, Config.secret, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        response = {}
+        response.update(
+            status=FAIL, message="Signature has expired", error=[], data={})
+        raise HTTPException(status_code=401, detail=response)
+    except jwt.InvalidTokenError as e:
+        print(e)
+        response = {}
+        response.update(status=FAIL, message="Invalid token",
+                        error=[], data={})
+        raise HTTPException(status_code=401, detail=response)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
