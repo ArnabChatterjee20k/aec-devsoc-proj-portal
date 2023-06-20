7f1bba654e1f2239d67f97c93d625c3140ad5a93
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-20 15:43:43 +0530

    bugs -- fixed parsing of object ids

--- api/v1/routes/project/project.py
+++ api/v1/routes/project/project.py
@@ -3,7 +3,8 @@ from .ProjectOwnerSchema import ProjectOwnerSchema
 from api.v1.utils.JWTBearer import JWTBearer
 from api.v1.Schemas.Project import ProjectDetails
 from api.v1.dependencies.isOwner import isOwner
-from api.v1.utils.project_utils import create_project , get_all_projects , get_project_by_id
+from api.v1.utils.project_utils import create_project, get_all_projects, get_project_by_id
+from beanie import PydanticObjectId
 
 router = APIRouter()
 
@@ -13,14 +14,15 @@ async def projects():
     return await get_all_projects()
 
 
-@router.get("/projects/:id")
-async def single_project(id:str):
+@router.get("/:id")
+async def single_project(id: PydanticObjectId):
+    PydanticObjectId(**id)
     return await get_project_by_id(id)
 
 
-@router.post("/projects", dependencies=[Depends(JWTBearer())])
-async def register_project(data:tuple=Depends(isOwner)):
-    user,details = data
+@router.post("/create", dependencies=[Depends(JWTBearer())])
+async def register_project(data: tuple = Depends(isOwner)):
+    user, details = data
     proj_id = await create_project(owner=user, data=details)
     response = {"message": "project_created"}
     if proj_id:
@@ -29,6 +31,7 @@ async def register_project(data:tuple=Depends(isOwner)):
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Some problem occured")
 
 
-@router.put("/projects/:id", dependencies=[Depends(JWTBearer)])
-def update_project():
+@router.put("/:id", dependencies=[Depends(JWTBearer())])
+def update_project(id: PydanticObjectId, data: tuple = Depends(isOwner)):
+    PydanticObjectId(**id)
     pass
