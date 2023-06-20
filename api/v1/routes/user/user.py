c21b62f7c784dc4b3e7af9021bd3e2440c8bdf05
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-20 15:35:09 +0530

    feat --- added routes for user related projects

--- api/v1/routes/user/user.py
+++ api/v1/routes/user/user.py
@@ -1,21 +1,32 @@
 from fastapi import APIRouter, Depends
 from api.v1.utils.JWTBearer import JWTBearer
 from api.v1.utils.user_utils import get_user_profile, edit_profile
+from api.v1.utils.project_utils import get_project_by_profile_id, get_project_of_a_user
 from api.v1.Schemas.Profile import ProfileEdit
 
-router = APIRouter(dependencies=[Depends(JWTBearer())])
+router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", dependencies=[Depends(JWTBearer())])
 async def user_profile(token: str = Depends(JWTBearer())):
     data = token.get("sub")
     email = data.get("email")
     return await get_user_profile(email=email)
 
 
-@router.put("/:id")
+@router.put("/:id", dependencies=[Depends(JWTBearer())])
 async def update_user(id: str, data: ProfileEdit, token: str = Depends(JWTBearer())):
     data = token.get("sub")
     email = data.get("email")
     await edit_profile(data=data, profile_id=id, email=email)
     return {"message": "sucess"}
+
+
+@router.get("/:user_id/projects/")
+async def user_projects(user_id):
+    return await get_project_by_profile_id(user_id)
+
+
+@router.get("/:user_id/projects/:project_id")
+async def user_projects(user_id, project_id):
+    return await get_project_of_a_user(user_id, project_id)
--- api/v1/utils/project_utils.py
+++ api/v1/utils/project_utils.py
@@ -2,6 +2,8 @@ from api.v1.database.models.Project import Project, SourceLink
 from api.v1.database.models.User import User
 from api.v1.Schemas.Project import ProjectDetails
 from beanie import PydanticObjectId
+from bson import ObjectId, errors
+from fastapi import HTTPException, status
 
 
 async def create_project(owner: User, data: dict):
@@ -20,4 +22,11 @@ async def get_project_by_id(id: PydanticObjectId):
 
 
 async def get_project_by_profile_id(id: str):
-    return await Project.find_all({"owner_id": id}).to_list()
+    return await Project.find({"owner_id": id}).to_list()
+
+
+async def get_project_of_a_user(user_id, project_id):
+    try:
+        return await Project.find_one({"owner_id": user_id, "_id": ObjectId(project_id)})
+    except errors.InvalidId as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST)
